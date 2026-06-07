046d84a4cd9e52f02998f4eb3d641a8f32823d10
Apichart_Ball <apichart.apcball@gmail.com>
2026-06-07 09:53:43 +0000

    ok

--- l10n_th_account_tax/models/purchase_order.py
+++ l10n_th_account_tax/models/purchase_order.py
@@ -1,7 +1,7 @@
 # Copyright 2026 Ecosoft Co., Ltd (https://ecosoft.co.th/)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
-from odoo import fields, models
+from odoo import fields, models, tools
 
 
 class PurchaseOrderLine(models.Model):
@@ -14,6 +14,14 @@ class PurchaseOrderLine(models.Model):
         domain="[('company_id', '=', company_id)]",
         help="Withholding tax to carry over to the vendor bill line.",
     )
+
+    def _auto_init(self):
+        """Ensure the stored WHT field exists even on databases that missed an upgrade."""
+        if not tools.column_exists(self.env.cr, self._table, "wht_tax_id"):
+            self.env.cr.execute(
+                f'ALTER TABLE "{self._table}" ADD COLUMN "wht_tax_id" integer'
+            )
+        return super()._auto_init()
 
     def _prepare_account_move_line(self, move=False):
         vals = super()._prepare_account_move_line(move=move)
